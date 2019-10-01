13db92ba297d88153212bb492c05493bec279a85
Christian Cain <christian22cain@yahoo.com>
2019-10-01 18:13:34 -0500

    cleaned scraper

--- lib/get_leads/scraper.rb
+++ lib/get_leads/scraper.rb
@@ -2,34 +2,23 @@ class GetLeads::Scraper
     attr_accessor :industry, :location, :amount
 
     def format_industry
+        #Formats industry input to be URL friendly
         self.industry.split(" ").join("+")
     end
 
     def format_location
-        #If you location.split.length is 2, that means city and state combo
-        if self.location.split(", ").length == 2
-            self.location.split(", ").join("%2C+")
-        else
-            self.location
-        end
+        #If location.split.length is 2, that means city and state combo. Else zipcode.
+        self.location.split(", ").length == 2 ? self.location.split(", ").join("%2C+") : self.location
     end
 
     def get_page
-        url_formatted_industry = format_industry
-        url_formatted_location = format_location
-        formatted_url = "https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"
-
+        formatted_url = "https://www.yellowpages.com/search?search_terms=#{format_industry}&geo_location_terms=#{format_location}"
         Nokogiri::HTML(open(formatted_url))
     end
 
-    def scrape_page
+    def scrape_page_results
         #TODO - only get x amount of results
         results = get_page.css('.organic .result')
-
-        results.each_with_index do |result, i|
-            new_lead = GetLeads::Lead.new(i)
-        end
-
     end
 
     def gen_leads(industry:, location:, amount:)
@@ -37,7 +26,7 @@ class GetLeads::Scraper
         @location = location
         @amount = amount
 
-        scrape_page
+        scrape_page_results
         puts "done"
     end
 end
