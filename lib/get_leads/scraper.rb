7d57edb0dc44ccb563990e24f6508500d615789c
Christian Cain <christian22cain@yahoo.com>
2019-10-01 17:51:27 -0500

    cleaned format_location

--- lib/get_leads/scraper.rb
+++ lib/get_leads/scraper.rb
@@ -6,29 +6,27 @@ class GetLeads::Scraper
     end
 
     def format_location
-        type = nil
         #If you location.split.length is 2, that means city and state combo
         if self.location.split(", ").length == 2
-            type = "city & state"
+            self.location.split(", ").join("%2C+")
         else
-            type = "zipcode"
+            self.location
         end
-        binding.pry
     end
 
     def get_page
-        url_formatted_industry = self.format_industry
+        url_formatted_industry = format_industry
         url_formatted_location = format_location
-        formatted_url = "https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"
 
         binding.pry
+        formatted_url = "https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"
 
-        #Template URL - https://www.yellowpages.com/search?search_terms=cbd+shop&geo_location_terms=Austin%2C+TX
-        #Nokogiri::HTML(open("https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"))
+        Nokogiri::HTML(open(formatted_url))
     end
 
     def scrape_page
-
+        doc = get_page
+        binding.pry
     end
 
     def gen_leads(industry:, location:, amount:)
@@ -36,7 +34,7 @@ class GetLeads::Scraper
         @location = location
         @amount = amount
 
-        doc = get_page
+        scrape_page
         binding.pry
         puts "Scraper init"
     end
