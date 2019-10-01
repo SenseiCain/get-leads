d28ee08564869fc5d9f3bed2d9f780e489757423
Christian Cain <christian22cain@yahoo.com>
2019-10-01 17:26:41 -0500

    added nogokiri

--- lib/get_leads/scraper.rb
+++ lib/get_leads/scraper.rb
@@ -1,12 +1,23 @@
 class GetLeads::Scraper
     attr_accessor :industry, :location, :amount
 
+    def get_page
+
+        #Template URL - https://www.yellowpages.com/search?search_terms=cbd+shop&geo_location_terms=Austin%2C+TX
+        Nokogiri::HTML(open("https://www.yellowpages.com/search?search_terms=cbd+shop&geo_location_terms=Austin%2C+TX"))
+    end
+
+    def scrape_page
+
+    end
+
     def gen_leads(industry:, location:, amount:)
         @industry = industry
         @location = location
         @amount = amount
 
-        #binding.pry
+        doc = get_page
+        binding.pry
         puts "Scraper init"
     end
 end
