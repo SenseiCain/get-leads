17e5e0249aca1bab5d751ffc49297bb473b65212
Christian Cain <christian22cain@yahoo.com>
2019-10-01 17:35:33 -0500

    extracted industry formatter

--- lib/get_leads/scraper.rb
+++ lib/get_leads/scraper.rb
@@ -1,10 +1,19 @@
 class GetLeads::Scraper
     attr_accessor :industry, :location, :amount
 
+    def format_industry
+        self.industry.split(" ").join("+")
+    end
+
     def get_page
-        url_formatted_industry = self.industry.split(" ").join("+")
+        url_formatted_industry = self.format_industry
+        url_formatted_location = nil
+        formatted_url = "https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"
+
+        binding.pry
+
         #Template URL - https://www.yellowpages.com/search?search_terms=cbd+shop&geo_location_terms=Austin%2C+TX
-        Nokogiri::HTML(open("https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"))
+        #Nokogiri::HTML(open("https://www.yellowpages.com/search?search_terms=#{url_formatted_industry}&geo_location_terms=Austin%2C+TX"))
     end
 
     def scrape_page
